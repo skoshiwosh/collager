9c86115f37ccd6fc53854f77b41df783091db577
Suzanne Berger <tangle1@mindspring.com>
2019-02-22 20:42:15 -0800

    Add files via upload

---
+++ collager.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#  collage.sh
+#  
+#
+#  Created by Suzanne Berger on 11/30/17.
+#
+MAYAPY=/Applications/Autodesk/maya2017/Maya.app/Contents/bin/mayapy
+CLLGRPY=/Users/suzanneberger/Documents/dev/collager/collager.py
+#
+$MAYAPY $CLLGRPY $*
