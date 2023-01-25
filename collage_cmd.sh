73bf72adfe7ec5ca6b2cc811863cf9e177fb1e30
Suzanne Berger <tangle1@mindspring.com>
2023-01-24 16:22:07 -0800

    use python3

--- collage_cmd.sh
+++ collage_cmd.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-#  collage_batch.sh
+#  collage_cmd.sh
 #  
 #
 #  Created by Suzanne Berger on 11/30/17.
 #
-MAYAPY=/Applications/Autodesk/maya2017/Maya.app/Contents/bin/mayapy
-CMDPY=/Users/suzanneberger/bin/collage_cmd.py
+#MAYAPY=/Applications/Autodesk/maya2018/Maya.app/Contents/bin/mayapy
+CMDPY=/Users/suzanneberger/Documents/dev/collager/collage_cmd.py
 #
-$MAYAPY $CMDPY $*
+python3 $CMDPY $*
